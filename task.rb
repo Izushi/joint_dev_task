54c24bde471533abf8831e36cd3327fd7e444b7e
EC2 Default User <ec2-user@ip-172-31-30-15.ap-northeast-1.compute.internal>
2020-11-04 22:16:03 +0000

    ruby assignment q1..q6

--- task.rb
+++ task.rb
@@ -5,7 +5,9 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  p names.push("斎藤")
+  
+  #p names << "斎藤" 
 end
 
 def q2
@@ -13,21 +15,30 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
-
+  p array = array1 + array2
+  
+  # array = array1 << array2
+  # p array.flatten!
 end
 
 def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
-
+  puts numbers.count(3)
 end
 
 def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-
+  
+  #破壊的メソッド
+  sports.compact! 
+  
+  # #deleteメソッド
+  # sports.delete(nil)
+  
   # 以下は変更しないで下さい
   p sports
 end
@@ -37,14 +48,28 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  puts array1.empty?
+  puts array2.empty?
 end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+  
+  #eachメソッド
+  numbers2 = []
+  numbers1.each do |number|
+    numbers2 << number * 10
+  end
+  p numbers2
+  
+  #mapメソッド
+  # numbers2 = numbers1.map do |number|
+  #   number * 10
+  # end
+  # p numbers2
+  
 end
 
 def q7
